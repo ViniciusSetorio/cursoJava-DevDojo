6ccac6c1b216a5941cd6014a5a00e512989bf8e9
Vinícius Alves <viniciusasetorio@gmail.com>
2024-06-20 10:57:01 -0300

    Curso de Java do DevDojo - Exceções

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest02 {
+    public static void main(String[] args) {
+        try {
+            divisao(1, 0);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Código final");
+    }
+
+    /**
+     * @param a
+     * @param b não pode ser zero
+     * @return
+     * @throws IllegalArgumentException caso b seja zero
+     */
+
+    private static int divisao(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Argumento ilegal, não pode ser 0");
+        }
+        return a / b;
+    }
+}
